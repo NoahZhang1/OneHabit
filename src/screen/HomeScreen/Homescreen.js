088f7ae4fa0ff46708ad5886465fb3b5211f1eb9
Yunnuo "Noah" Zhang <zyn920@126.com>
2023-03-09 15:29:28 -0500

    add calendar view to HomeScreen

--- src/screen/HomeScreen/Homescreen.js
+++ src/screen/HomeScreen/Homescreen.js
@@ -1,25 +1,64 @@
 import React from 'react';
-import { View, Text } from 'react-native';
+import { View, Text, StyleSheet, SafeAreaView, TouchableOpacity } from 'react-native';
 import { Auth } from 'aws-amplify';
+import { Calendar, CalendarList, Agenda, calendarTheme, AgendaEntry } from 'react-native-calendars';
 
 const HomeScreen = () => {
 
+
+    // const vacation = { key: 'vacation', color: 'red', selectedDotColor: 'blue' };
+    // const massage = { key: 'massage', color: 'blue', selectedDotColor: 'blue' };
+    // const workout = { key: 'workout', color: 'green' };
+    // const selected_date = '2023-03-08'
     return (
-        <View style={{ flex: 1 }}>
-            <Text style={{ fontSize: 24, alignSelf: 'center' }}>Home, sweet home</Text>
-            <Text
-                style={{
-                    width: '100%',
-                    textAlign: 'center',
-                    color: 'red',
-                    marginTop: 'auto',
-                    marginVertical: 20,
-                    fontSize: 20,
-                }}>
-                Sign Out
-            </Text>
-        </View>
+
+        <SafeAreaView style={styles.container}>
+            <Agenda
+                selected='2023-03-08'
+                // selected = selected_date
+                items={{
+                    '2023-03-09': [{ name: 'CS 1332 PLUS session' }, { name: 'President Talk' }, { name: 'CRC Meditation Session' }],
+                    '2023-03-08': [{ name: 'PHYS 2212 Studio' }]
+                }}
+                renderItem={(item, isFirst) => (
+                    <TouchableOpacity style={styles.item}>
+                        <Text style={styles.itemText}>{item.name}</Text>
+                    </TouchableOpacity>
+                )}
+
+                renderEmptyDate={() => (
+
+                    <TouchableOpacity style={styles.item}>
+                        <Text>This is empty date!</Text>
+                    </TouchableOpacity>
+                )}
+            />
+        </SafeAreaView>
     );
 };
 
 export default HomeScreen;
+
+const styles = StyleSheet.create({
+    container: {
+        flex: 1,
+        justifyContent: 'center'
+    },
+    item: {
+        backgroundColor: 'white',
+        flex: 1,
+        borderRadius: 5,
+        padding: 10,
+        marginRight: 10,
+        marginTop: 17,
+    },
+    itemText: {
+        color: '#888',
+        fontSize: 16,
+    },
+    emptyDate: {
+        height: 15,
+        flex: 1,
+        paddingTop: 30
+    }
+});
