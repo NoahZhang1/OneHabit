b6f220789435f71e7074c05f6924180d4d1ea5d3
Ryan <ryanjamessequeira@gmail.com>
2023-03-09 22:39:50 -0500

    Add leaderboard ui and increment functionality

--- src/screen/LeaderboardScreen/LeaderboardScreen.js
+++ src/screen/LeaderboardScreen/LeaderboardScreen.js
@@ -1,22 +1,50 @@
-import React from 'react';
-import { View, Text } from 'react-native';
+import React, { useState } from 'react';
+import { View, ScrollView, Text, TextInput, Button } from 'react-native';
 import { Auth } from 'aws-amplify';
 import Leaderboard from 'react-native-leaderboard';
-this.state = {
-    data: [
-        {userName: 'Joe', highScore: 52},
-        {userName: 'Jenny', highScore: 120},
-        //...
-    ] //can also be an object of objects!: data: {a:{}, b:{}}
-}
-const LeaderboardScreen = () => {
 
+
+const LeaderboardScreen = () => {
+    const [data, handleState] = useState([
+        { userName: 'Joe', score: 52 },
+        { userName: 'Jenny', score: 120 },
+        { userName: 'Alfred', score: 29 },
+        { userName: 'Hannah', score: 0 },
+        { userName: 'Kate', score: 19 },
+        { userName: 'Michael', score: 22 },
+    ]
+    );
+
+    const [increment, handleIncrement] = useState(0);
     return (
-        <Leaderboard 
-        data={this.state.data} 
-        sortBy='highScore' 
-        labelBy='userName'/>
-        
+        <ScrollView>
+            <Text style={{ fontSize: 22, textAlign: 'center' }}>{"\n\nLeaderboard\n\n"}</Text>
+            <Leaderboard
+                data={data}
+                sortBy='score'
+                labelBy='userName' />
+            <TextInput
+                style={{
+                    height: 40, width: 100, borderWidth: 1, padding: 5,
+                    margin: 10, alignSelf: 'center',
+                    color: 'black', placeholderTextColor: 'black'
+                }}
+                editable
+                placeholder='Enter Hours'
+                onChangeText={(text) => handleIncrement(parseInt(text))} />
+            <Button
+                title='Submit'
+                style={{
+                    height: 40, width: 300, borderWidth: 1, padding: 5,
+                    margin: 10, alignSelf: 'center',
+                    color: 'black',
+                }}
+                onPress={() => { var temp = data.slice(); temp[2].score += increment; handleState(temp)}}
+            >
+
+            </Button>
+
+        </ScrollView>
     );
 };
 
