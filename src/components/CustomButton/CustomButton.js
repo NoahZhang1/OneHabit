4dc84e72b666663381a7eeb2edbb61c2567c7249
Yunnuo "Noah" Zhang <zyn920@126.com>
2023-03-28 23:38:47 -0400

    commit message

---
+++ src/components/CustomButton/CustomButton.js
@@ -0,0 +1,62 @@
+import React from 'react'
+import { View, Text, StyleSheet, Pressable } from 'react-native'
+
+
+const CustomButton = ({ onPress, text, type = 'PRIMARY', bgColor, fgColor }) => {
+    return (
+        <Pressable
+            onPress={onPress}
+            style={[
+                styles.container,
+                styles[`container_${type}`],
+                bgColor ? { backgroundColor: bgColor } : {}
+            ]}>
+            <Text
+                style={[
+                    styles.text,
+                    styles[`text_${type}`],
+                    fgColor ? { color: fgColor } : {},
+                ]}
+            >
+                {text}
+            </Text>
+        </Pressable>
+    )
+}
+
+const styles = StyleSheet.create({
+    container: {
+
+
+        width: '30%',
+
+        padding: 15,
+        marginVertical: 5,
+
+        alignItems: 'center',
+        borderRadius: 5,
+
+    },
+
+    container_PRIMARY: {
+        backgroundColor: '#3B71F3',
+        marginVertical: 60
+    },
+
+    container_TERTIARY: {
+
+        width: '100%'
+    },
+
+
+    text: {
+        fontWeight: 'bold',
+        color: 'white'
+    },
+
+    text_TERTIARY: {
+        color: 'gray'
+    }
+})
+
+export default CustomButton
