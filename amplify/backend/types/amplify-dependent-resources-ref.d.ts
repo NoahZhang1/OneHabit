ecb9fce207beb309848c3436136febc2968e6fcd
Yunnuo "Noah" Zhang <zyn920@126.com>
2023-03-28 20:49:47 -0400

    commit message

---
+++ amplify/backend/types/amplify-dependent-resources-ref.d.ts
@@ -0,0 +1,13 @@
+export type AmplifyDependentResourcesAttributes = {
+  "auth": {
+    "onehabit7b1c2f82": {
+      "AppClientID": "string",
+      "AppClientIDWeb": "string",
+      "IdentityPoolId": "string",
+      "IdentityPoolName": "string",
+      "UserPoolArn": "string",
+      "UserPoolId": "string",
+      "UserPoolName": "string"
+    }
+  }
+}
---
+++ src/screen/SettingScreen/SettingScreen.js
@@ -0,0 +1,34 @@
+import React from 'react';
+import { View, Text, Button } from 'react-native';
+import { Auth } from 'aws-amplify';
+
+// write an empty screen for setting screen and add a button to navigate to it
+const SettingScreen = () => {
+
+
+    const signOut = () => {
+        Auth.signOut();
+    };
+
+    return (
+
+
+        <View style={{ flex: 1 }}>
+            <Text style={{ fontSize: 24, alignSelf: 'center' }}>Home, sweet home</Text>
+            <Text
+                onPress={signOut}
+                style={{
+                    width: '100%',
+                    textAlign: 'center',
+                    color: 'red',
+                    marginTop: 'auto',
+                    marginVertical: 20,
+                    fontSize: 20,
+                }}>
+                Sign out
+            </Text>
+        </View>
+    );
+}
+
+export default SettingScreen;
