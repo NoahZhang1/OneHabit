116f8c1852e83469af8af5da91a5b20baa0e8497
Ryan <ryanjamessequeira@gmail.com>
2023-04-16 16:00:48 -0400

    Add Datastore functionality for ClassSelectScreen

--- src/screen/ClassSelectScreen/ClassSelectScreen.js
+++ src/screen/ClassSelectScreen/ClassSelectScreen.js
@@ -1,32 +1,66 @@
-import React from 'react';
-import {View, StyleSheet, Dimensions} from "react-native";
-import {Title, Text, Button, Chip, Snackbar, Portal, TextInput} from "react-native-paper";
+import React, { useState, useEffect } from 'react';
+import { View, StyleSheet, Dimensions } from "react-native";
+import { Title, Text, Button, Chip, Snackbar, Portal, TextInput, Divider } from "react-native-paper";
 import { Logs } from 'expo'
 import { DataStore } from '@aws-amplify/datastore';
 import { Classes } from '../../models';
-
+import { Auth } from 'aws-amplify';
 
 Logs.enableExpoCliLogging()
 const screenWidth = Dimensions.get("window").width;
 
-function GoalScreen({navigation}) {
+function ClassSelectScreen({ navigation }) {
 
     const [targetSnackVisible, setTargetSnackVisible] = React.useState(false);
     const onToggleTargetSnackBar = () => setTargetSnackVisible(true);
     const onDismissTargetSnackBar = () => setTargetSnackVisible(false);
 
+    const [classes, setClasses] = useState([{ className: 'Loading Classes', goal: '0 Hours' }]);
+    const [user, handleUser] = useState('');
+    const [newClass, addClass] = useState(null);
+    const [newGoal, addGoal] = useState(null);
 
 
-    React.useEffect(() => {
-        // fetch from database
-    }, []);
-
-    // Show the congrat message
-    React.useEffect(() => {
-        if (targetReach===true) {
-            onToggleTargetSnackBar();
+    function pushData(classData, goalData) {
+        console.log('push')
+        const pushChange = async () => {
+            console.log(classData)
+            await DataStore.save(new Classes({
+                "username": user,
+                "className": classData,
+                "goal": goalData,
+                "progress": 0
+            }));
+            console.log('pushed')
         }
-    }, [targetReach])
+        pushChange()
+    }
+    useEffect(() => {
+        const getUser = async () => {
+            var temp = await Auth.currentUserInfo();
+            handleUser(temp["username"])
+            console.log(user)
+        }
+
+        getUser();
+    }, []);
+    useEffect(() => {
+        const pullData = async () => {
+            var classData = await DataStore.query(Classes, (c) => c.username.eq(user));
+            console.log(classData)
+            console.log("CLASS DATA")
+            setClasses(classData)
+        }
+        pullData()
+    }, [user])
+
+    //logging
+    useEffect(() => {
+        console.log("Changed Log")
+        console.log(user);
+        console.log(classes)
+        console.log("End Changed Log")
+    }, [classes, user])
 
 
 
@@ -34,25 +68,61 @@ function GoalScreen({navigation}) {
         <View style={styles.container}>
             <View style={styles.content}>
                 <Title style="text-align:left">Set Your Study Hours</Title>
-                <Title style={{fontSize:16, textAlign:'center', paddingLeft:20, paddingRight:20}}>How many hours would you like to study for each class this week?</Title>
-                    {true ? (<View style={styles.addContainer}>
-                    <Text style={{fontSize:20, fontWeight:'bold'}}>CS 3510</Text>
+                <Title style={{ fontSize: 16, textAlign: 'center', paddingLeft: 20, paddingRight: 20 }}>How many hours would you like to study for each class this week?</Title>
+                {true ? (<View style={styles.addContainer}>
+                    <TextInput
+                        returnKeyType='done'
+                        keyboardType='text'
+                        placeholder='Class'
+                        onChangeText={a => addClass(a)}
+                    />
                     <TextInput
                         returnKeyType='done'
                         keyboardType='decimal-pad'
-                        placeholder='in hours'
-                        onChangeText={console.log('a')}
-                    /><View style={styles.buttons}>
+                        placeholder='Goal (hours)'
+                        onChangeText={a => addGoal(parseInt(a))}
+                    />
+                    
+                    <View style={styles.buttons}>
                         <Button mode="contained"
                             onPress={() =>
-                                [
-                                onToggleTargetSnackBar()
-                                
-                            ]
+                                {
+                                    
+                                    if (!newClass || !newGoal) {
+                                        console.log('one input not entered')
+                                    } else {
+                                        if (classes.map(a => {return a['className']}).indexOf(newClass) != -1) {
+                                            console.log('duplicate');
+                                        } else {
+                                            console.log('test1')
+                                            let temp = classes.slice()
+                                            temp.push({ className: newClass, goal: newGoal })
+                                            console.log(temp)
+                                            console.log(temp)
+                                            setClasses(temp)
+                                            pushData(newClass, newGoal)
+                                            console.log('test2')
+                                            onToggleTargetSnackBar()
+                                            console.log('test3')
+                                        }
+                                    }
+                                    
+                                    
+                                }
                             }>Submit</Button>
-                    </View></View>) : <Text style={{fontWeight:'bold'}}>Class Time Submitted!</Text>}
-                    
-                
+                    </View></View>) : <Text style={{ fontWeight: 'bold' }}>Class Time Submitted!</Text>}
+
+                <Title style="text-align:left">Current Classes</Title>
+                <View style={styles.content}>
+                    {classes.map(a => {
+                        return (
+                            <View style={styles.addContainer}>
+                                <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{a.className}</Text>
+                                <Text style={{ fontSize: 20, fontWeight: 'bold' }}>{a.goal}</Text>
+                            </View>
+                        );
+                    })}
+                </View>
             </View>
             <Snackbar
                 visible={targetSnackVisible}
@@ -94,13 +164,13 @@ const styles = StyleSheet.create({
     buttons: {
         flexDirection: 'row',
         alignItems: 'center',
-        width: screenWidth-100,
+        width: screenWidth - 100,
         alignContent: 'space-between',
         flexWrap: 'wrap',
         justifyContent: 'space-evenly',
     },
-    
-  
+
+
 });
 
-export default GoalScreen;
+export default ClassSelectScreen;
